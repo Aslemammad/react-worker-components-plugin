d36a7637786c8d1bb5a53a46f46a0d93c0525101
aslemammad <zorofight94@gmail.com>
2022-01-03 12:03:09 +0330

    second commit

---
+++ plugin/rollup.config.js
@@ -0,0 +1,55 @@
+import esbuild from 'rollup-plugin-esbuild'
+import dts from 'rollup-plugin-dts'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+import alias from '@rollup/plugin-alias'
+import pkg from './package.json'
+
+const entry = [
+  'src/vite.ts',
+]
+
+const external = [
+  ...Object.keys(pkg.dependencies || []),
+  ...Object.keys(pkg.peerDependencies || []),
+]
+
+export default [
+  {
+    input: entry,
+    output: {
+      dir: 'dist',
+      format: 'esm',
+    },
+    external,
+    plugins: [
+      alias({
+        entries: [
+          { find: /^node:(.+)$/, replacement: '$1' },
+        ],
+      }),
+      resolve({
+        preferBuiltins: true,
+      }),
+      json(),
+      commonjs(),
+      esbuild({
+        target: 'node14',
+      }),
+    ],
+  },
+  {
+    input: [
+      'src/vite.ts',
+    ],
+    output: {
+      file: 'dist/vite.d.ts',
+      format: 'esm',
+    },
+    external,
+    plugins: [
+      dts(),
+    ],
+  },
+]
